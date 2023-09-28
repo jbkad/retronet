51ceab69fedae4518c0a16c464f5d16ab77f923c
jbkad <jbkadibu@gmail.com>
2023-09-28 23:44:53 +0100

    Commit 2.1 - Navbar fixes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,6 @@ import { useContext } from 'react';
 import { ReactComponent as MenuIcon } from '../assests/MenuIcon.svg';
 import { ReactComponent as LightThemeIcon } from '../assests/LightThemeIcon.svg';
 import { ReactComponent as DarkThemeIcon } from '../assests/DarkThemeIcon.svg';
-// import { navbar_icon, lightTheme_icon, darkTheme_icon } from '../styles/Constants';
 import { ThemeContext } from './ThemeToggle';
 import '../styles/Navbar.css';
 
@@ -12,13 +11,13 @@ export function Navbar() {
 
     return (
         <nav className='navbar'>
-            <div className='navbar__container'>
-                <h1 className='navbar__title'>
+            <div className='container'>
+                <h1 className='title'>
                     <MenuIcon />
                     Retro Net
                 </h1>
                 <div
-                    className='navbar__toggle'
+                    className='toggle'
                     onClick={() => toggleTheme()}
                 >
                     {theme === "dark-theme" ? (
