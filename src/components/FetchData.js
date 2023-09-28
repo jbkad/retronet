b77f184159229d126172fc508bba6e17ff7e0ea3
jbkad <jbkadibu@gmail.com>
2023-09-28 18:59:20 +0100

    Commit 1.7 - Link fix

--- src/components/FetchData.js
+++ src/components/FetchData.js
@@ -3,7 +3,7 @@ export function fetchArchiveData(websiteUrl, currentDate) {
     const tenYearsAgo = new Date(currentDate.getFullYear() - 10, currentDate.getMonth(), currentDate.getDate());
     const formattedDate = tenYearsAgo.toISOString().slice(0, 10).replace(/-/g, "");
     
-    const apiUrl = `http://archive.org/wayback/available?url=${websiteUrl}&timestamp=${formattedDate}`;
+    const apiUrl = `https://archive.org/wayback/available?url=${websiteUrl}&timestamp=${formattedDate}`;
 
     return fetch(apiUrl)
         .then(response => response.json())
