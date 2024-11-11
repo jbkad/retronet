9c06f7dfb0f88fefa3cd5fa4ec1333c3da855156
jbkad <jbkadibu@gmail.com>
2024-11-11 13:50:07 +0000

    del(index): themes removed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ThemeProvider } from './components/ThemeToggle';
+
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
@@ -10,9 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <ThemeProvider>
         <App />
-      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
